88109c8b22db2dbb43e2fc37444a60e6b9485890
James Sinkala <xinnks@gmail.com>
2022-09-26 10:49:03 +0300

    feat: Add `/categories` endpoint

---
+++ endpoints/categories.ts
@@ -0,0 +1,3 @@
+import { Category } from "../models/Category";
+
+export default Category.crud()
---
+++ models/Category.ts
@@ -0,0 +1,5 @@
+import { ChiselEntity } from "@chiselstrike/api"
+
+export class Category extends ChiselEntity {
+  name: string
+}
