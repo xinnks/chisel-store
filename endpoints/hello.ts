8dd73c48d8a33efb47439424933d8b2fb6d15a4f
James Sinkala <xinnks@gmail.com>
2022-11-11 14:23:18 +0300

    feat: Update and move hello route file to /routes directory. Implementation of breaking change in ChiselStrike from v0.12 to v0.13

--- endpoints/hello.ts
+++
@@ -1,9 +0,0 @@
-// Example ChiselStrike endpoint.
-//
-// To access the endpoint, run:
-//
-// curl -d '{"hello": "world"}' localhost:8080/dev/hello
-
-export default async function (req: Request): Promise<string> {
-    return await req.text() || "hello world";
-}
